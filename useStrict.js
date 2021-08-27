d64fd4fae463770df582896c738c172c945431bc
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-08-27 09:16:39 +0200

    first commit

---
+++ useStrict.js
@@ -0,0 +1,11 @@
+"use strict";
+const bool = "true";
+const num = 41;
+const str = "Peter";
+const obj = {
+  cats: 2,
+  cars: 1,
+};
+const nothing = null;
+let undf;
+const symbol = Symbol("symbol");
